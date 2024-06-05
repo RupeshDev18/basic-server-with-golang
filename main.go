04b190948ecece2500c985bca6cb9dfba542eec4
Rupesh <112386480+Rupesh180902@users.noreply.github.com>
2024-06-05 23:06:32 +0530

    Implement Guestbook functionality and enhanced UI.
    - Guestbook Logic:
      - Create a `GuestbookEntry` struct to store guest information (name, message).
      - Implement form handling to capture and validate user input.
      - Store submitted entries in a slice (`guestbookEntries`).
      - Implement logic to retrieve and display guestbook entries on a separate page.
    - UI Improvements:
      - Design stylish CSS files (`guestList.css` and `style.css`) for a visually appealing interface.
      - Create separate HTML pages (`guest.html` and `guestList.html`) for Guestbook and guest list views.
      - Refactor `index.html` to maintain a clean structure and user flow.
      - Integrate a button for smooth navigation between pages after a successful submission.

--- main.go
+++ main.go
@@ -4,8 +4,52 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
 
+type GuestbookEntry struct {
+	Name    string `json:"name"`
+	Message string `json:"message"`
+}
+
+var guestbookEntries []GuestbookEntry
+
+var tmpl *template.Template
+
+func init() {
+	var err error
+	tmpl, err = template.ParseFiles("./static/guestList.html")
+	if err != nil {
+		log.Fatal("Error parsing template:", err)
+	}
+}
+func getGuestList(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	name := r.FormValue("name")
+	message := r.FormValue("message")
+
+	if name == "" || message == "" {
+		fmt.Fprintf(w, "Please enter your name and message.")
+		return
+	}
+	entry := GuestbookEntry{Name: name, Message: message}
+	guestbookEntries = append(guestbookEntries, entry)
+
+	fmt.Fprintf(w, "Thank you for signing the guestbook!")
+}
+func guestListHandler(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+		GuestbookEntries []GuestbookEntry
+	}{
+		GuestbookEntries: guestbookEntries,
+	}
+	if err := tmpl.Execute(w, data); err != nil {
+		log.Fatal("Error executing template:", err)
+	}
+}
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
@@ -35,7 +79,8 @@ func main() {
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
-
+	http.HandleFunc("/addGuest", getGuestList)
+	http.HandleFunc("/guestList", guestListHandler)
 	fmt.Printf("Starting server at port 8080 \n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
